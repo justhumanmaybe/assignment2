fb55413b14609fac169bb5b504adc71017ad69ec
Anson Ng <ansonng129@gmail.com>
2022-06-20 20:15:56 -0400

    add edit

--- Server/Config/app.js
+++ Server/Config/app.js
@@ -44,10 +44,10 @@ let localStrategy = passport_local_1.default.Strategy;
 const user_1 = __importDefault(require("../Models/user"));
 var app = (0, express_1.default)();
 const DBConfig = __importStar(require("./db"));
-mongoose_1.default.connect(DBConfig.RemoteURI);
+mongoose_1.default.connect(DBConfig.RemoteURI || DBConfig.LocalURI);
 const db = mongoose_1.default.connection;
 db.on("open", function () {
-    console.log(`Connected to MongoDB at: ${DBConfig.HostName}`);
+    console.log(`Connected to MongoDB at: ${(DBConfig.RemoteURI) ? DBConfig.HostName : "LocalHost"} `);
 });
 db.on("error", function () {
     console.log(`Connection Error`);
--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://127.0.0.1/business_contact";
-exports.RemoteURI = "mongodb+srv://anson:IRJlygt7VurzIJY9@cluster0.nzrwf.mongodb.net/media?retryWrites=true&w=majority";
+exports.RemoteURI = process.env.RemoteURI;
 exports.HostName = "MongoDB Atlas";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
